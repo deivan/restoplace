da7ecc39dc157b71c1bd298664f24a68300c2f8b
Ivan Demenkov <ariastation@ua.fm>
2022-05-06 18:38:20 +0300

    added City selector

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+const CITIES = {
+  dnipro: 'Дніпро',
+  kyiv: 'Київ',
+  lviv: 'Львів'
+};
+
 window.addEventListener('load', () => {
+  new App();
   console.log('app started.');
 });
+
+class App {
+  constructor() {
+    this.selectedCity = '';
+    this.initEvents();
+  }
+
+  initEvents() {
+    const citySelector = document.querySelector('#citySelector');
+
+    citySelector.addEventListener('click', e => {
+      this.selectedCity = e.target.dataset.city || '';
+      if (this.selectedCity) document.querySelector('#selectedCity').innerText = CITIES[this.selectedCity]
+    });
+  }
+}
